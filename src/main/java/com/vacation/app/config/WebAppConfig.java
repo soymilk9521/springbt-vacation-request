7daef4ef8e49a7fb1219dc46a6b1d223355d73d2
r_li <r_li@globals.jp>
2017-12-27 18:45:25 +0900

    internationalization

---
+++ src/main/java/com/vacation/app/config/WebAppConfig.java
@@ -0,0 +1,67 @@
+package com.vacation.app.config;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+	
+	@Value("${lang.default.lang}")
+    private String defLang;
+
+    @Value("${lang.default.locale}")
+    private String defLocal;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    /*
+     * locale change interceptor
+     */
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    /*
+     * sets default locale
+     */
+    @Bean
+    public CookieLocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        Locale defautLocal = new Locale(defLang + "_"  + defLocal);
+        localeResolver.setDefaultLocale(defautLocal);
+        return localeResolver;
+    }
+
+    /*
+     * customize message location
+     */
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        // set *.properties file default encoding
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+        messageSource.setBasename("classpath:i18n/messages");
+        return messageSource;
+    }
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
