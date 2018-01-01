8576f9026683bfca5e39f23cf0e4e65b518c1ff8
soymilk9521 <soymilk9521@gmail.com>
2018-01-01 22:13:59 +0900

    remember me setting

--- src/main/java/com/vacation/app/config/WebSecurityConfig.java
+++ src/main/java/com/vacation/app/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
 
 import com.vacation.app.service.CustomUserService;
 
@@ -28,18 +29,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/css/**", "/scripts/**", "/img/**").permitAll();
+		http.authorizeRequests().antMatchers("/apply").fullyAuthenticated(); // open login page when session is changed 
 		http.authorizeRequests().anyRequest().authenticated()
         .and().formLogin().loginPage("/login")
         // 设置默认登录成功跳转页面
-         .defaultSuccessUrl("/index").failureUrl("/login?error").permitAll()
+        .defaultSuccessUrl("/index").failureUrl("/login?error").permitAll()
         .and()
         // 开启cookie保存用户数据
         .rememberMe()
-        .useSecureCookie(true)
+        // .useSecureCookie(true) // only https effect
         // 设置cookie有效期
         .tokenValiditySeconds(60 * 60 * 24 * 7)
+        .tokenRepository(new InMemoryTokenRepositoryImpl()) //check cookie
         // 设置cookie的私钥
-        // .key("")
+        // .key("c295bWlsazk1MjE=") // base64 encoded string
         .and()
         .logout()
         // 默认注销行为为logout，可以通过下面的方式来修改
